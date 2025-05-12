9850dc0de727d22a4ac91177dee888ed1d01fefc
Kirill <polyncevkirill924@gmail.com>
2025-05-12 17:12:26 +0700

    Программа для обмена информации через FIFO между родственными процессами

---
+++ pr010.c
@@ -0,0 +1,58 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#define BYTES 44
+
+int main() {
+	int fd, result;
+	size_t size;
+	char resstring[BYTES];
+	char name[]="file.fifo";
+	/*
+	if(mknod(name, S_IFIFO | 0664, 0) < 0) {
+		printf("не удалось создать файл FIFO\n");
+		exit(-1);
+	}
+	*/
+	if ((result = fork()) < 0) {
+		printf("Не удалось создать дочерный процесс\n");
+		exit(-1);
+	}
+	else if (result > 0) {
+		if((fd = open(name, O_WRONLY))<0) {
+			printf("не удалось открыть FIFO на запись\n");
+			exit(-1);
+		}
+		size = write(fd, "Текстовая строка для FIFO", BYTES);
+		if (size != BYTES) {
+			printf("не удалось записать всю строку в FIFO\n");
+			exit(-1);
+		}
+		if(close(fd)<0) {
+			printf("Не удалось закрыть FIFO\n");
+			exit(-1);
+		}
+		printf("Родитель записал строку в FIFO и завершает работу\n");
+	}
+	else {
+		if((fd = open(name, O_RDONLY)) <0) {
+			printf("Не удалось открыть FIFO на чтение\n");
+			exit(-1);
+		}
+		size = read(fd, resstring, BYTES);
+		if (size <0) {
+			printf("Не удалось прочитать строки из FIFO\n");
+			exit(-1);
+		}
+		printf("содержание прочитаной строки: %s\n", resstring); 
+		if (close(fd) <0) {
+			printf("Не удалось закрыть FIFO\n");
+			exit(-1);
+		}
+	}
+	return 0;
+}
+
